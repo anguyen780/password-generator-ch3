daffa1d058de812f02178b134751f708afe0ab2a
Lux <anguyen780@gmail.com>
2022-08-31 19:22:26 -0500

    created a generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,42 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    var totalChar = ''
+    var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
+    var numbers = "1234567890";
+    var symbols = "\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~";
+    var generatedPassword = '';
+    var passLength = prompt("How long would you like your password? Must be a number between 8-128");
+    if ((parseInt(passLength) >= 8) && (parseInt(passLength) <= 128)){
+
+    } 
+    else {
+        alert("Password length must be a number between 8-128");
+        return;
+    }
+    var confirmUpper = confirm('Would you like to include uppercase characters?');
+    if (confirmUpper){
+        totalChar = totalChar + upperChar;
+    }
+    var confirmLower = confirm('Would you like to include lowercase characters?');
+    if (confirmLower){
+        totalChar = totalChar + lowerChar;
+    }
+    var confirmNumber = confirm('Would you like to include numbers?');
+    if (confirmNumber){
+        totalChar = totalChar + numbers;
+    }
+    var confirmSymbol = confirm('Would you like to include symbols?');
+    if (confirmSymbol){
+        totalChar = totalChar + symbols;
+    }
+    for (var i = 0; i < parseInt(passLength); i++){
+        var randomPass = totalChar[Math.floor(Math.random()*totalChar.length)];
+        generatedPassword = generatedPassword + randomPass;
+    }
+    return generatedPassword;
+}
+
